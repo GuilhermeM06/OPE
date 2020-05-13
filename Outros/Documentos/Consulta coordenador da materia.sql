9ee52109a86d9f3a869211fa7023e056163accb4
Douglas Silva <douglas.silva@pactbr.com>
2020-05-13 19:59:07 -0300

    Limpando repositório

--- Outros/Documentos/Consulta coordenador da materia.sql
+++
@@ -1,4 +0,0 @@
-use TSQL2012
-
-SELECT D.Nome AS 'Disciplina', C.Nome AS 'Nome do Coordenador', C.Email AS 'Email para Contato', C.Celular AS 'Celular para Contato'
-FROM Disciplina AS D JOIN Coordenador AS C ON D.IdCoordenador = C.ID
